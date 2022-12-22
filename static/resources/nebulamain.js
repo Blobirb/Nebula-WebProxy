b3bb02c5c16d18f360f9b651b964736355dfd71d
Blobirb <98100850+Blobirb@users.noreply.github.com>
2022-12-21 22:13:37 -0500

    Update nebulamain.js

--- static/resources/nebulamain.js
+++ static/resources/nebulamain.js
@@ -125,7 +125,7 @@ window.addEventListener("load", () => {
         .then(() => {
           const value = event.target.firstElementChild.value
           let url = value.trim()
-          if (!isUrl(url)) url = "https://www.google.com/search?q=" + url
+          if (!isUrl(url)) url = "https://duckduckgo.com/?q=" + url + "&t=h_&ia=web"
           if (!(url.startsWith("https://") || url.startsWith("http://")))
             url = "http://" + url
           // encode the URL for UltraViolet
